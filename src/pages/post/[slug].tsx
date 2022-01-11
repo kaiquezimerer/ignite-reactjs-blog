cef73f7ec34c7d3f1258a74c24c2d0b2d6077845
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-01-11 19:51:37 -0300

    Render post page by slug + styles

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,11 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+/* eslint-disable react/no-danger */
 
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -29,83 +34,88 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
+export default function Post({ post }: PostProps): JSX.Element {
   return (
     <>
       <div className={commonStyles.container}>
         <Header />
       </div>
       <article className={styles.content}>
-        <section className={styles.cover} />
+        <section
+          className={styles.banner}
+          style={{ backgroundImage: `url(${post.data.banner.url})` }}
+        />
         <section className={commonStyles.container}>
-          <h1>Criando um app CRA do zero</h1>
+          <h1>{post.data.title}</h1>
           <div className={commonStyles.postInfo}>
             <time>
               <FiCalendar />
-              15 Mar 2021
+              {post.first_publication_date}
             </time>
             <h4>
               <FiUser />
-              Joseph Oliveira
+              {post.data.author}
             </h4>
             <h4>
               <FiClock />4 min
             </h4>
           </div>
-          <h2>Proin et varius </h2>
-          <p>
-            Proin et varius Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Nullam dolor sapien, vulputate eu diam at, condimentum
-            hendrerit tellus. Nam facilisis sodales felis, pharetra pharetra
-            lectus auctor sed. Ut venenatis mauris vel libero pretium, et
-            pretium ligula faucibus. Morbi nibh felis, elementuaposuere et,
-            vulputate et erat. Nam venenatis. Cras laoreet mi Nulla auctor sit
-            amet quam vitae commodo. Sed risus justo, vulputate quis neque eget,
-            dictum sodales sem. In eget felis finibus, mattis magna a, efficitur
-            ex. Curabitur vitae justo consequat sapien gravida auctor a non
-            risus. Sed malesuada mauris nec orci congue, interdum efficitur urna
-            dignissim. Vivamus cursus elit sem, vel facilisis nulla pretium
-            consectetur. Nunc congue. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Aliquam
-            consectetur massa nec metus condimentum, sed tincidunt enim
-            tincidunt. Vestibulum fringilla risus sit amet massa suscipit
-            eleifend.
-          </p>
-          <h2>Proin et varius </h2>
-          <p>
-            Proin et varius Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Nullam dolor sapien, vulputate eu diam at, condimentum
-            hendrerit tellus. Nam facilisis sodales felis, pharetra pharetra
-            lectus auctor sed. Ut venenatis mauris vel libero pretium, et
-            pretium ligula faucibus. Morbi nibh felis, elementum a posuere et,
-            vulputate et erat. Nam venenatis. Cras laoreet mi Nulla auctor sit
-            amet quam vitae commodo. Sed risus justo, vulputate quis neque eget,
-            dictum sodales sem. In eget felis finibus, mattis magna a,{' '}
-            <a href="#">efficiturex</a> . Curabitur vitae justo consequat sapien
-            gravida auctor a non risus. Sed malesuada mauris nec orci congue,
-            interdum efficitur urna dignissim. Vivamus cursus elit sem, vel
-            facilisis nulla pretium consectetur. Nunc congue. Class aptent
-            taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-            himenaeos. Aliquam consectetur massa nec metus condimentum, sed
-            tincidunt enim tincidunt. Vestibulum fringilla risus sit amet massa
-            suscipit eleifend.
-          </p>
+          {post.data.content.map(elem => (
+            <>
+              <h2>{elem.heading}</h2>
+              <div dangerouslySetInnerHTML={{ __html: elem.body[0].text }} />
+            </>
+          ))}
         </section>
       </article>
     </>
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  // // TODO
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async props => {
+  const { slug } = props.params;
 
-//   // TODO
-// };
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd LLL yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content.map(item => {
+        return {
+          heading: item.heading,
+          body: [{ text: RichText.asHtml(item.body) }],
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 30 * 60,
+  };
+};
